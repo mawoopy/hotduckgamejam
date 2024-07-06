561efb164e8d7272b1efdb5ddfa9c23d24706397
mariewispler <sallyjeanjenkins@gmail.com>
2024-07-06 20:25:47 +0200

    level is demoable

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayButton()
     {
-        SceneManager.LoadScene("Lake");
+        SceneManager.LoadScene("MarieLevel");
     }
 
     public void CreditsButton()
